fcc4a28b459ad80325d9e97397527a91f3a34430
kNagadou <kzms33_4@icloud.com>
2018-04-02 14:37:11 +0900

    Initial Commit

---
+++ KNTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KNTableView
+//
+//  Created by nagado-kazumasa on 2018/04/02.
+//  Copyright © 2018年 nagado-kazumasa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
